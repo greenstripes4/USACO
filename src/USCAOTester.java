c59d28c879c03b0f902aa8bada378b4ce5a24321
greenstripes4 <strongheart404@gmail.com>
2019-08-31 21:03:02 -0700

    Support additonal test folder and execution time output.

--- src/USCAOTester.java
+++ src/USCAOTester.java
@@ -6,64 +6,71 @@ import java.nio.file.Files;
 
 public class USCAOTester {
     public static void main(String[] args) throws IOException {
-        final String problem = "pageant";
+        final String problem = "gifts";
         File targetInput = new File(problem + ".in");
         File targetOutput = new File(problem + ".out");
-        for (int i = 1; i <= 20; i++) {
-            File input = new File("test\\" + problem + "\\" + i+".in");
-            if(!input.exists()) {
-                input = new File("test\\" + problem + "\\I." + i);
-            }
-            File output = new File("test\\" + problem + "\\" + i+".out");
-            if(!output.exists()) {
-                output = new File("test\\" + problem + "\\O." + i);
-            }
-            if(!input.exists()) {
-                continue;
-            }
+        String[] tests = {"ray-test\\", "test\\"};
 
-            if (targetInput.exists()) {
-                targetInput.delete();
-            }
-            if (targetOutput.exists()) {
-                targetOutput.delete();
-            }
-            Files.copy(input.toPath(), targetInput.toPath());
-            Main.main(null);
-
-            BufferedReader reader1 = new BufferedReader(new FileReader(targetOutput));
-            BufferedReader reader2 = new BufferedReader(new FileReader(output));
-            String line1 = reader1.readLine();
-            String line2 = reader2.readLine();
-            boolean areEqual = true;
-            int lineNum = 1;
-            while (line1 != null || line2 != null) {
-                if (line1 == null || line2 == null) {
-                    areEqual = false;
-                    break;
-                } else if (!line1.equals(line2)) {
-                    areEqual = false;
-                    break;
+        for(String test : tests) {
+            for (int i = 1; i <= 20; i++) {
+                File input = new File(test + problem + "\\I." + i);
+                if (!input.exists()) {
+                    input = new File(test + problem + "\\" + i + ".in");
                 }
-                line1 = reader1.readLine();
-                line2 = reader2.readLine();
-                lineNum++;
-            }
+                File output = new File(test + problem + "\\O." + i);
+                if (!output.exists()) {
+                    output = new File(test + problem + "\\" + i + ".out");
+                }
+                if (!input.exists()) {
+                    continue;
+                }
 
-            if (areEqual) {
-                System.out.println("I." + i + " test passed");
-            } else {
-                System.out.println("I." + i + " test failed");
-                //System.out.println("Expected " + line1 + " and got " + line2 + " at line " + lineNum);
-                break;
-            }
-            reader1.close();
-            reader2.close();
-            if (targetInput.exists()) {
-                targetInput.delete();
-            }
-            if (targetOutput.exists()) {
-                targetOutput.delete();
+                if (targetInput.exists()) {
+                    targetInput.delete();
+                }
+                if (targetOutput.exists()) {
+                    targetOutput.delete();
+                }
+                Files.copy(input.toPath(), targetInput.toPath());
+                long start = System.nanoTime();
+                Main.main(null);
+                long end = System.nanoTime();
+
+                BufferedReader reader1 = new BufferedReader(new FileReader(targetOutput));
+                BufferedReader reader2 = new BufferedReader(new FileReader(output));
+                String line1 = reader1.readLine();
+                String line2 = reader2.readLine();
+                boolean areEqual = true;
+                int lineNum = 1;
+                while (line1 != null || line2 != null) {
+                    if (line1 == null || line2 == null) {
+                        areEqual = false;
+                        break;
+                    } else if (!line1.equals(line2)) {
+                        areEqual = false;
+                        break;
+                    }
+                    line1 = reader1.readLine();
+                    line2 = reader2.readLine();
+                    lineNum++;
+                }
+
+                reader1.close();
+                reader2.close();
+                if (targetInput.exists()) {
+                    targetInput.delete();
+                }
+                if (targetOutput.exists()) {
+                    targetOutput.delete();
+                }
+
+                if (areEqual) {
+                    System.out.println(test + ":" + i + " test passed in " + ((end - start) / 1000000) + "ms");
+                } else {
+                    System.out.println(test + ":" + i + " test failed in " + ((end - start) / 1000000) + "ms");
+                    //System.out.println("Expected " + line1 + " and got " + line2 + " at line " + lineNum);
+                    System.exit(0);
+                }
             }
         }
     }
